fabb53a3230e618e110985d938ed58c1005801d7
starsidea <director_9414@outlook.com>
2016-04-30 16:37:35 +0300

    upgrade index.js  commited on2016-04-30T16:37:35+03:00

---
+++ utility/index.js
@@ -0,0 +1,27 @@
+/*!
+
+=========================================================
+* Material Dashboard PRO React - v1.8.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-pro-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React, { useReducer } from "react";
+import ReactDOM from "react-dom";
+
+import MainScreen from "./MainScreen.js";
+
+ReactDOM.render(
+  <div>
+    <MainScreen />
+  </div>,
+  document.getElementById("root")
+);
